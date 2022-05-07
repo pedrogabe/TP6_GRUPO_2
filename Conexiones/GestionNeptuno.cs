60206755a7d2637a135d05a546d91b7fbf9484aa
AgusR-99 <agus.socialclub@gmail.com>
2022-05-07 01:16:03 -0300

    Organizar archivos en carpeta "Conexiones". Agregar "Gestion Neptuno.cs"

---
+++ Conexiones/GestionNeptuno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace TP6_GRUPO2
+{
+    public class GestionNeptuno
+    {
+        // Constructor
+        public GestionNeptuno() { }
+
+        // Metodos
+        private DataTable ObtenerTabla(string query)
+        {
+            try
+            {
+                DataSet ds = DB.DBClass.Query(query);
+                return ds.Tables[0];
+            }
+            catch (ArgumentNullException)
+            {
+                System.Diagnostics.Trace.WriteLine("Invalid query");
+                throw;
+            }
+        }
+        public DataTable ObtenerTodosLosProductos()
+        {
+            return ObtenerTabla("SELECT * FROM Productos");
+        }
+    }
+}
